a203d55e267b2fc1d9a67797f802a7aabfd95153
johne <johne@URIEL-PC>
2017-12-09 19:31:20 -0500

    Datos Final

--- Practica002/src/practica002/controlador/EventoPais.java
+++ Practica002/src/practica002/controlador/EventoPais.java
@@ -7,6 +7,11 @@ package practica002.controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import practica002.modelo.Pais;
 import practica002.vista.VentanaPais;
@@ -27,46 +32,71 @@ public class EventoPais  implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e) 
     {
+        FileWriter fw = null;
        try{
-        String nombre = this.ventanaPais.getTxtList().get(0).getText();
-       String continente = this.ventanaPais.getTxtList().get(1).getText();
-       int codigo = Integer.parseInt(this.ventanaPais.getTxtList().get(2).getText());
-       
-        Pais pais = new Pais(nombre,continente,codigo);
-        
-        boolean bandera = true;
-
-            int i = 0;
-
-            for (Pais a : this.ventanaPais.getGestionDato().getPaisList()) {
-
-                if (pais.getCodigo() == this.ventanaPais.getGestionDato().getPaisList().get(i).getCodigo()) {
-
-                    JOptionPane.showMessageDialog(null, "Datos ya existentes");
-                    bandera = false;
-                    break;
-                }
-                i++;
-            }
-             if (bandera == true) {
-
-                this.ventanaPais.getGestionDato().addPais(pais);
-
-            }
+           try{
+               String nombre = this.ventanaPais.getTxtList().get(0).getText();
+               String continente = this.ventanaPais.getTxtList().get(1).getText();
+               int codigo = Integer.parseInt(this.ventanaPais.getTxtList().get(2).getText());
+               Pais pais = new Pais(nombre,continente,codigo);
+               boolean bandera = true;
+               
+               int i = 0;
+               
+               for (Pais a : this.ventanaPais.getGestionDato().getPaisList()) {
+                   
+                   if (pais.getCodigo() == this.ventanaPais.getGestionDato().getPaisList().get(i).getCodigo()) {
+                       
+                       JOptionPane.showMessageDialog(null, "Datos ya existentes");
+                       bandera = false;
+                       break;
+                   }
+                   i++;
+               }
+               if (bandera == true) {
+                   this.ventanaPais.getGestionDato().addPais(pais);
+               }
+           }
+           catch (NumberFormatException a) {
+               
+               JOptionPane.showMessageDialog(null, "Llene correctamente los datos");
+               
+           }
+           this.ventanaPais.getTxtList().get(0).setText("");
+           this.ventanaPais.getTxtList().get(1).setText("");
+           this.ventanaPais.getTxtList().get(2).setText("");
+           Object[][] datoAsistente = this.ventanaPais.cargaDatosTabla(this.ventanaPais.getGestionDato().getPaisList().size(), 3);
+           this.ventanaPais.setDatos(datoAsistente);
+           this.ventanaPais.getModeloTabla().setDataVector(this.ventanaPais.getDatos(), this.ventanaPais.getEncabezado());
+          //Agregar Archivo Ubicacion 
+          fw = new FileWriter("C:\\carpetaPractica02\\Pais.txt");
+          BufferedWriter bw=new BufferedWriter(fw);
+          System.err.println("Datos");
+          for (Pais a : this.ventanaPais.getGestionDato().getPaisList())
+          {
+              //dato agregado
+              bw.append(a.toString());
+              bw.newLine();
+              //System.out.println(a.getNombre()+"|"+a.getContinente()+"|"+a.getCodigo());
+              
+          }
+           bw.close();
        }
-       catch (NumberFormatException a) {
+       catch (IOException ex) {
 
-            JOptionPane.showMessageDialog(null, "Llene correctamente los datos");
+            Logger.getLogger(EventoPais.class.getName()).log(Level.SEVERE,null, ex);
 
-        }
-       this.ventanaPais.getTxtList().get(0).setText("");
-        this.ventanaPais.getTxtList().get(1).setText("");
-        this.ventanaPais.getTxtList().get(2).setText("");
-       
-        Object[][] datoAsistente = this.ventanaPais.cargaDatosTabla(this.ventanaPais.getGestionDato().getPaisList().size(), 3);
-        this.ventanaPais.setDatos(datoAsistente);
-        this.ventanaPais.getModeloTabla().setDataVector(this.ventanaPais.getDatos(), this.ventanaPais.getEncabezado());
+        } finally {
+          try {
+              fw.close();
+          } catch (IOException ex) {
+              Logger.getLogger(EventoPais.class.getName()).log(Level.SEVERE, null, ex);
+          }
+      }
+        
          
+         
+ 
     }
 
     public VentanaPais getVentanaPais() {
